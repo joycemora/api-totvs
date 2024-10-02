eec7bee2f972c50107088c9aadb7e883dbe35327
joyce <joycemoraes608@gmail.com>
2024-10-01 23:43:33 -0300

    bugfix: pequenas correcoes no recurso do ambientes

--- db.js
+++ db.js
@@ -1,15 +1,15 @@
-const mysql = require("msql2/promise");
+const mysql = require("mysql2/promise");
 
 const client = mysql.createPool(process.env.CONNECTION_STRING)
 
  async function selectUsers() {
     const results = await client.query("SELECT * FROM usuarios:");
-    return results(0);
+    return results[0];
 }
 
 async function selectUser(id) {
     const results = await client.query("SELECT * FROM usuarios WHERE id=?", [id]);
-    return results(0);
+    return results[0];
 }
 
 async function insertUser(user) {
@@ -27,23 +27,31 @@ async function insertUser(user) {
 }
 
 async function selectEnvironments() {
-    const results = await client.query("SELECT * FROM ambientes:");
-    return results(0);
+    const results = await client.query("SELECT * FROM ambientes");
+    return results[0];
 }
 
 async function selectEnvironment(id) {
-    const results = await client.query("SELECT * FROM ambientes WHERE uuid=?", [id]);
-    return results(0);
+    const results = await client.query("SELECT * FROM ambientes WHERE id=?", [id]);
+    return results[0][0];
 }
 
 async function insertEnvironment(environment) {
-    const values = [environment.id, environment.active, environment.softwareType,environment.expirationDate];
-    await client.query("INSERT INTO ambientes(id,active,softwareType,expirationDate) VALUES(?,?,?,?)", values);
+    if(environment.softwareType === "erp")stype=environment.softwareType;
+    else stype ="sgbd"
+    await client.query("INSERT INTO ambientes(software_type) VALUES(?)", stype);
 }
 
  async function updateEnvironment(id,environment ) {
-    const querySql = "UPDATE ambientes SET active=?,softwareType=?,expirationDate=? WHERE id=?";
-    const values = [environment.active, environment.softwareType,environment.expirationDate,id];
+    const atual = await client.query("SELECT * FROM ambientes WHERE id=?", [id]);
+    const date1 = new Date(environment.expirationDate);
+    const date2 = new Date(atual[0][0].expiration_date);
+    if(date1.getTime() > date2.getTime())xpDate=environment.expirationDate;
+   else xpDate = date2;
+
+    if(environment.active == null)environment.active=true;
+    const querySql = "UPDATE ambientes SET active_=?,expiration_date=? WHERE id=?";
+    const values = [environment.active,xpDate,id];
     await client.query(querySql, values);
 }
 
--- index.js
+++ index.js
@@ -12,67 +12,67 @@ app.delete("/usuarios/:id", async (request, response) => {
     const id = parseInt(request.params.id);
     await db.deleteUser(id);
     response.sendStatus(204);
-})
+});
 
 app.path("/usuarios/:id", async (request, response) => {
     const id = parseInt(request.params.id);
     const user = request.body;
     await db.updateUser(id, user);
     response.sendStatus(200);
-})
+});
 
 app.post("/usuarios/", async (request, response) => {
     const user = request.body;
     await db.insertUser(user);
     response.sendStatus(201);
-})
+});
 
 app.get("/usuarios/:id", async (request, response) => {
     const id = parseInt(request.params.id);
     const results = await db.selectUsers(id)
     response.json(results)
-})
+});
 
 app.get("/usuarios", async (request, response) => {
     const results = await db.selectUsers
     response.json(results);
-})
+});
 
 app.delete("/ambientes/:id", async (request, response) => {
     const id = parseInt(request.params.id);
     await db.deleteEnvironment(id);
     response.sendStatus(204);
-})
+});
 
-app.path("/ambientes/:id", async (request, response) => {
+app.patch("/ambientes/:id", async (request, response) => {
     const id = parseInt(request.params.id);
     const environment= request.body;
     await db.updateEnvironment(id, environment);
     response.sendStatus(200);
-})
+});
 
 app.post("/ambientes/", async (request, response) => {
     const environment = request.body;
     await db.insertEnvironment(environment);
     response.sendStatus(201);
-})
+});
 
 app.get("/ambientes/:id", async (request, response) => {
     const id = parseInt(request.params.id);
     const results = await db.selectEnvironment(id)
     response.json(results)
-})
+});
 
 app.get("/ambientes", async (request, response) => {
-    const results = await db.selectEnvironments
+    const results = await db.selectEnvironments()
     response.json(results);
-})
+});
 
-app.get("/", (request, response, next) => {
+app.get("/", (request, response) => {
     response.json({
         message: "It's alive"
     })
-})
+});
 
 app.listen(process.env.PORT, () => {
     console.log("App  now is running!");
